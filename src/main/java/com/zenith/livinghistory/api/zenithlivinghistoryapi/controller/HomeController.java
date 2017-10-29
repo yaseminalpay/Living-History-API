d4057ff53618b12b36ed74f1daf8698779c6c0ec
Gokce Yucel <gkcycl@gmail.com>
2017-10-29 22:03:24 +0300

    Arbitrary update to test end-to-ent continuous deployment 2

--- src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/controller/HomeController.java
+++ src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/controller/HomeController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String home() {
-        return "This is a Living-History Api container running on Google Cloud";
+        return "This is Living-History Api running on Google Cloud";
     }
 }
